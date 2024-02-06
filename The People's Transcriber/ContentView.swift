d492294d13e9e77eb3d488e65d8403f28ce1a9e0
Steve Rogers <srogers@bio.unc.edu>
2024-02-06 16:14:56 -0500

    Initial Commit

---
+++ The People's Transcriber/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  The People's Transcriber
+//
+//  Created by Landon Rogers on 2/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ The People's Transcriber/The_People_s_TranscriberApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_People_s_TranscriberApp.swift
+//  The People's Transcriber
+//
+//  Created by Landon Rogers on 2/6/24.
+//
+
+import SwiftUI
+
+@main
+struct The_People_s_TranscriberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
